5896939b88c7a05c54dc6ee34615dda50aa12396
Jakes Potgieter <jakespotgieter@gmail.com>
2020-06-23 17:36:11 +0100

    Retrofit - Project restructure

--- src/FileConverter/Configuration.cs
+++ src/FileConverter/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace FileConverter
 {
+    using System;
     using System.IO;
+    using System.Threading.Tasks;
     using Microsoft.Extensions.Configuration;
 
     public class Configuration : IConfiguration
@@ -27,6 +29,16 @@
         public string GetPluginDirectory()
         {
            return this._config.GetSection("PluginDirectory").Value;
+        }
+
+
+        private void DownloadPlugins(string url)
+        {
+            string downloadLocation = Path.GetTempPath() + Path.DirectorySeparatorChar + "Converter";
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.DownloadFile(new Uri(url), @"c:\temp\myfile.txt");
+            }
         }
     }
 }
